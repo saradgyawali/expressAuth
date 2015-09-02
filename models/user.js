f48d94b2646c12e870725461343983f18e12d045
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-09-02 11:16:43 -0700

    third commit

--- models/user.js
+++ models/user.js
@@ -1,3 +1,16 @@
+//Dependencies
 var mongoose = require('mongoose'),
 Schema = mongoose.Schema,
 bcrypt = require('bcrypt');
+
+// The user Schema
+var UserSchema = new Schema({
+  email: {type: String, required: true},
+  passwordDigest: {type: String, required: true},
+  createdAt: {type: Date, default: Date.now}
+});
+
+// define user model
+var User = mongoose.model('User', UserSchema);
+// export user model
+module.exports = User;
